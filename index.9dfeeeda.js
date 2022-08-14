f443116b18c74950b5cefd2c2c3b08050b2801e3
meretska-sofiia <107505622+meretska-sofiia@users.noreply.github.com>
2022-08-14 19:05:03 +0000

    Deploying to gh-pages from @ meretska-sofiia/project-html-css@a6b12ae04c19332d74901cd5164e6a8dd7a338b6 🚀

---
+++ index.9dfeeeda.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,arrows:!1})})),$(".slider__button-right").on("click",(function(){$(".slider").slick("slickNext")})),$(".slider__button-left").on("click",(function(){$(".slider").slick("slickPrev")}));
+//# sourceMappingURL=index.9dfeeeda.js.map
